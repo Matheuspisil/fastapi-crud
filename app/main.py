87f9073928c9b9bfac3cae5c2840cc7233509c6a
Matheuspisil <ajudaserio@yahoo.com.br>
2024-10-31 22:35:07 +0000

    Salvando mudanças locais

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from . import models, crud, database
+from app.database import get_db
+
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=database.engine)
+
+@app.get("/")
+def read_root():
+    return {"message": "Desafio Fastapi JabutiAGI"}
 
 @app.get("/usuarios/")
 def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(database.get_db)):
@@ -20,8 +26,9 @@ def read_user(user_id: int, db: Session = Depends(database.get_db)):
 
 @app.post("/usuarios/")
 def create_user(nome: str, email: str, idade: int, db: Session = Depends(database.get_db)):
-    user = models.User(nome=nome, email=email, idade=idade)
-    return crud.create_user(db, user)
+    new_user = models.User(nome=nome, email=email, idade=idade)
+    user = crud.create_user(db, new_user)
+    return user
 
 @app.put("/usuarios/{user_id}")
 def update_user(user_id: int, nome: str, email: str, idade: int, db: Session = Depends(database.get_db)):
@@ -32,7 +39,11 @@ def update_user(user_id: int, nome: str, email: str, idade: int, db: Session = D
 
 @app.delete("/usuarios/{user_id}")
 def delete_user(user_id: int, db: Session = Depends(database.get_db)):
-    success = crud.delete_user(db, user_id)
-    if not success:
+    user = crud.get_user(db, user_id)
+    if user is None:
         raise HTTPException(status_code=404, detail="Usuário não encontrado")
-    return {"message": "Usuário deletado com sucesso"}
+    
+    success = crud.delete_user(db, user_id)
+    if success:
+        return {"message": "Usuário deletado com sucesso"}
+    raise HTTPException(status_code=500, detail="Erro ao tentar deletar o usuário")
